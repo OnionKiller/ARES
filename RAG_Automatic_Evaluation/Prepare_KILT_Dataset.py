f55be6a3c88258b1f57b87712a43f980ad45f546
Jon Saad-falcon <jsaad@Jons-MacBook-Pro.local>
2023-11-21 22:28:27 -0500

    update

--- RAG_Automatic_Evaluation/Prepare_KILT_Dataset.py
+++ RAG_Automatic_Evaluation/Prepare_KILT_Dataset.py
@@ -57,12 +57,12 @@ print("Finished loading KILT wiki!")
 
 ####################################################################
 
-dataset_choices = ["nq"] #"nq", "hotpotqa", "wow", "fever", 
+dataset_choices = ["nq", "hotpotqa", "wow", "fever"] #"nq", "hotpotqa", "wow", "fever", 
 process_wikipedia = False
 gather_full_wikipedia_articles = False
 
-#positive_negative_ratios = [0.7, 0.725, 0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.9]
-positive_negative_ratios = [0.5, 0.525, 0.55, 0.575, 0.6, 0.625, 0.65, 0.675, 0.7]
+positive_negative_ratios = [0.7, 0.725, 0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.9]
+#positive_negative_ratios = [0.5, 0.525, 0.55, 0.575, 0.6, 0.625, 0.65, 0.675, 0.7]
 
 ####################################################################
 

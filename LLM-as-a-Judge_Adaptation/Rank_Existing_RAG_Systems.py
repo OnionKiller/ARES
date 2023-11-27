8f82c9c2dc20affee4aa2d83f93626e236386a5a
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-11-26 20:21:14 -0800

    update

--- LLM-as-a-Judge_Adaptation/Rank_Existing_RAG_Systems.py
+++ LLM-as-a-Judge_Adaptation/Rank_Existing_RAG_Systems.py
@@ -173,10 +173,11 @@ class RAG_System:
                     )
                     indexer = Indexer(checkpoint="/future/u/jonsf/msmarco.psg.kldR2.nway64.ib__colbert-400000", config=config)
                     indexer.index(name=index_path, collection=collection, overwrite=True)
+                    breakpoint()
                     index_path = indexer.get_index()
                     
-            searcher = Searcher(index=index_path, collection=collection)
-            self.retriever = searcher
+                    searcher = Searcher(index=index_path, collection=collection)
+                    self.retriever = searcher
 
         """elif self.retriever_selection == "facebook/rag-sequence-nq":
             dataframe = cfg[2].drop_duplicates(subset="Document")
